59773073bd56d77b736db24070e96541db1ad853
Lakshay Kumar <imlakshaykumar@gmail.com>
2024-01-15 11:37:01 +0530

    env issue check

--- service.js
+++ service.js
@@ -3,8 +3,14 @@ const app_id = import.meta.env.VITE_APP_ID
 const api_key = import.meta.env.VITE_API_KEY
 
 export const fetchData = async (defaultQuery) => {
-    const data = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${defaultQuery}&app_id=${app_id}&app_key=${api_key}`)
+
+    const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${defaultQuery}&app_id=${app_id}&app_key=${api_key}`;
+    console.log(url);
+
+    const data = await fetch(url);
+
     const res = await data.json();
     return res.hits;
+
 }
 
