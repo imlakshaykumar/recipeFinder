3534e695a2cd266caae5d85fd564c41c84d7c582
Lakshay Kumar <imlakshaykumar@gmail.com>
2024-01-15 12:13:50 +0530

    fixed env

--- src/service.js
+++ src/service.js
@@ -1,8 +1,8 @@
 
 
 export const fetchData = async (defaultQuery) => {
-    const app_id = import.meta.env.VITE_MY_APP_ID
-    const api_key = import.meta.env.VITE_MY_API_KEY
+    const app_id = import.meta.env.VITE_VERCEL_MY_APP_ID
+    const api_key = import.meta.env.VITE_VERCEL_MY_API_KEY
 
     const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${defaultQuery}&app_id=${app_id}&app_key=${api_key}`;
     console.log(url);
